9908cbec3166a5afef316b9ef8044e0c59e59bb8
GiriBabu <117948742+giribabulearning@users.noreply.github.com>
2022-11-17 16:15:38 -0500

    Update streamlit_app.py
    Added code to normalize and display the JSON response in table

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,9 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
+
+# normalize JSON response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display the results in a table
+streamlit.dataframe(fruityvice_normalized)
